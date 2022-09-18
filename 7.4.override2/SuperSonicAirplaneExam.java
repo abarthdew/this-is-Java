17a40829a2dba2df51c1b4112ef64192bdaebcae
ABarthDew <vsevolod117@gmail.com>
2022-09-18 20:14:46 +0900

    7. updated

---
+++ 7.4.override2/SuperSonicAirplaneExam.java
@@ -0,0 +1,12 @@
+public class SuperSonicAirplaneExam {
+    public static void main(String[] args) {
+        SupersonicAirplane SuperSonicAirplane = new SupersonicAirplane();
+        SuperSonicAirplane.fly();
+        /*
+         일반비행
+        초음속 모드로 변경
+        초음속 모드로 비행
+         */
+        // 자식 메서드 재정의 안에 부모 메서드를 불러 함께 사용 가능
+    }
+}
