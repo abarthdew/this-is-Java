af1ad26bc8fd487a94e5031261790e0211304b94
ABarthDew <vsevolod117@gmail.com>
2022-09-26 22:24:12 +0900

    8. updated

--- 9.3.nestedAccess/NestedA2.java
+++ 9.3.nestedAccess/NestedA2.java
@@ -20,7 +20,7 @@ public class NestedA2 {
 
     // 정적 메서드
     static void method2() {
-        NestedB2 var1 = new NestedB2(); // 에러: method2()가 static이라 A 객체 없이도 사용 가능한데, B는 불가능
+        // NestedB2 var1 = new NestedB2(); // 에러: method2()가 static이라 A 객체 없이도 사용 가능한데, B는 불가능
         NestedC2 var2 = new NestedC2(); // NestedC2 클래스는 static이므로 A 객체 생성 없이 사용 가능
     }
 }
