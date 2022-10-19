7a06fd5571852450bc24691ecd3222d1cd7d25de
ABarthDew <vsevolod117@gmail.com>
2022-10-19 23:48:00 +0900

    11. updated

---
+++ 11.7.string/ByteToStringExam.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class ByteToStringExam {
+    public static void main(String[] args) throws IOException {
+        
+        byte[] bytes1 = {72, 101, 108, 108, 111, 32, 74, 97, 118, 97};
+        String str1 = new String(bytes1);
+        System.out.println(str1); // Hello Java
+
+        byte[] bytes2 = new byte[100];
+        System.out.print("입력: ");
+        int readByteNo = System.in.read(bytes2);
+        String str2 = new String(bytes2, 0, readByteNo - 2);
+        System.out.println(str2);
+        /*
+        입력: java
+        java
+         */
+
+    }
+}
---
+++ 11.7.string/CharAtExam.java
@@ -0,0 +1,19 @@
+public class CharAtExam {
+    public static void main(String[] args) {
+        
+        String ssn = "012345-1231231";
+        char sex = ssn.charAt(7);
+        System.out.println(sex); // 1
+
+        switch(sex) {
+            case '1':
+                System.out.println("남자");
+                break;
+            case '3':
+            case '2':
+            case '4':
+                System.out.println("여자");
+                break;
+        } // 남자
+    }
+}
